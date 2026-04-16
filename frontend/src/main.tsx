d15b1e2a1bf6a233bea7b825d023eb56804ff031
sucary <78484823+sucary@users.noreply.github.com>
2026-04-16 23:26:17 +0300

    Initialize localization

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -7,22 +8,25 @@ import { SettingsPage } from './components/Settings/SettingsPage'
 import { AboutPage } from './components/AboutPage'
 import { AuthProvider } from './context/AuthContext'
 import { LocationLanguageProvider } from './context/LocationLanguageContext'
+import i18n from './i18n';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <AuthProvider>
-      <LocationLanguageProvider>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/u/:username" element={<App />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </LocationLanguageProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  </QueryClientProvider>,
+    <I18nextProvider i18n={i18n}>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <AuthProvider>
+                    <LocationLanguageProvider>
+                        <Routes>
+                            <Route path="/" element={<App />} />
+                            <Route path="/u/:username" element={<App />} />
+                            <Route path="/settings" element={<SettingsPage />} />
+                            <Route path="/about" element={<AboutPage />} />
+                        </Routes>
+                    </LocationLanguageProvider>
+                </AuthProvider>
+            </BrowserRouter>
+        </QueryClientProvider>
+    </I18nextProvider>,
 );
